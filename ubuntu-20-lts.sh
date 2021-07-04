bc71d248cc087e4d38ec147ef32a52d22bf6e0cf
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2021-07-04 23:05:52 +0200

    Update ubuntu-20-lts.sh

--- ubuntu-20-lts.sh
+++ ubuntu-20-lts.sh
@@ -62,6 +62,32 @@ sudo apt-get install -y mssql-server
 # PostgreSQL
 sudo apt install -y postgresql postgresql-contrib
 
+# Docker runtime
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+
+sudo usermod -a -G docker "$USER"
+
+# jq is useful for inspecting Docker containers' JSON
+sudo apt-get -y jq
+
 ###################################################
 ################## GUI TOOLS ######################
 ###################################################
