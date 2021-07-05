2f2de6b3a49ad0ff23d56c1ef154ca8e069a490f
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2021-07-05 23:48:02 +0200

    Update ubuntu-20-lts.sh

--- ubuntu-20-lts.sh
+++ ubuntu-20-lts.sh
@@ -51,13 +51,16 @@ sudo apt-get -y install dotnet-sdk-5.0
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
+
 # npm without sudo
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
+
 echo "
-#custom npm binaries
+# custom npm binaries
 export PATH=~/.npm-global/bin:$PATH
 " >> ~/.bashrc
+
 source ~/.bashrc
 
 # Golang
