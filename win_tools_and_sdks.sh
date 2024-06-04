4156734d39c98a4e9395f250698ca466d0d16b8e
Michał Wilczyński <michal@wilczynski.pro>
2024-06-04 10:15:02 +0200

    Create win_tools_and_sdks.sh

---
+++ win_tools_and_sdks.sh
@@ -0,0 +1,11 @@
+winget install Microsoft.DotNet.DesktopRuntime.6 -a x86
+winget install Microsoft.DotNet.SDK.8
+winget install Python.Python
+winget install OpenJS.NodeJS
+
+winget install Microsoft.SQLServerManagementStudio
+winget install 7zip.7zip
+winget install Microsoft.VisualStudioCode
+winget install "azure data studio"
+winget install Notepad++.Notepad++
+winget install Microsoft.AzureCLI
